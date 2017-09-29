4bcdec11dce39694dbba7b58400b2c09636e37f9
Nataraj Sindam <nasindam@microsoft.com>
2017-09-28 18:16:35 -0700

    first commit

---
+++ ClientApp/app/components/blog/blog.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'blog',
+    templateUrl: './blog.component.html',
+    styleUrls: ['./blof.component.css']
+})
+export class BlogComponent {
+}
